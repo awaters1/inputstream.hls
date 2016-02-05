3132c0ac495a939237b61da54c2e60efca0f2596
xbmc <fernetmenta@online.de>
2016-02-05 15:05:57 +0100

    initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,60 @@
+/*
+ *      Copyright (C) 2008-2013 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "libXBMC_addon.h"
+
+extern "C" {
+
+#include "kodi_inputstream_dll.h"
+
+ADDON::CHelper_libXBMC_addon *XBMC = nullptr;
+
+ADDON_STATUS ADDON_Create(void* hdl, void* props)
+{
+  if (!XBMC)
+    XBMC = new ADDON::CHelper_libXBMC_addon;
+
+  if (!XBMC->RegisterMe(hdl))
+  {
+    delete XBMC, XBMC=nullptr;
+    return ADDON_STATUS_PERMANENT_FAILURE;
+  }
+
+  return ADDON_STATUS_OK;
+}
+
+//-- Stop ---------------------------------------------------------------------
+// This dll must cease all runtime activities
+// !!! Add-on master function !!!
+//-----------------------------------------------------------------------------
+void ADDON_Stop()
+{
+}
+
+//-- Destroy ------------------------------------------------------------------
+// Do everything before unload of this add-on
+// !!! Add-on master function !!!
+//-----------------------------------------------------------------------------
+void ADDON_Destroy()
+{
+  XBMC=NULL;
+}
+
+}
